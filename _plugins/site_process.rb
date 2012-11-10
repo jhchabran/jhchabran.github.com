2e985af1bb7a343d40777014df7fd788100ae29e
Jean-Hadrien Chabran <jh@chabran.fr>
2012-11-10 04:05:10 -0800

    Add archives, categories and tags

---
+++ _plugins/site_process.rb
@@ -0,0 +1,17 @@
+module Jekyll
+  class Site
+    def process
+      self.reset
+      self.read
+      self.generate
+      self.render
+
+      # these must come after render
+      self.generate_tags_categories
+      self.generate_archives
+
+      self.cleanup
+      self.write
+    end
+  end
+end
