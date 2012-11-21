c0990f9c9b53b1aa7d5597480c187a897015c4bd
Jean-Hadrien Chabran <jh@chabran.fr>
2012-11-21 01:03:50 -0800

    Add menu highlighting for posts

--- js/application.js
+++ js/application.js
@@ -1,13 +1,8 @@
 !function ($) {
   $(function(){
     $("ul.app-menu li").each(function() {
-      var href = $(this).find('a').attr("href");
-
-      if(href == window.location.pathname)
+      if($(this).find('a').attr("href") == window.location.pathname)
         $(this).addClass("active"); 
-      else
-        if(window.location.pathname.indexOf(href) == 0 && href != '/')
-          $(this).addClass("active"); 
     });
   });
 }(window.jQuery);
