c8aab6eee66f95fd4278c7299cf0ce92a30d414b
Jean-Hadrien Chabran <jh@chabran.fr>
2012-11-21 01:05:05 -0800

    WERWQERQEW

--- js/application.js
+++ js/application.js
@@ -1,8 +1,13 @@
 !function ($) {
   $(function(){
     $("ul.app-menu li").each(function() {
-      if($(this).find('a').attr("href") == window.location.pathname)
+      var href = $(this).find('a').attr("href");
+
+      if(href == window.location.pathname)
         $(this).addClass("active"); 
+      else
+        if(window.location.pathname.indexOf(href) == 0 && href != '/')
+          $(this).addClass("active"); 
     });
   });
 }(window.jQuery);
