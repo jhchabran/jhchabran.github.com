7c9bdf2c52dfc62e7d92515b33d1df0149d34f8f
Jean-Hadrien Chabran <jh@chabran.fr>
2012-12-12 07:47:47 -0800

    Track Amazon affiliates

--- _site/js/application.js
+++ _site/js/application.js
@@ -1,4 +1,13 @@
 !function ($) {
+  // Google Analytics
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '{{site.google_analytics}}']);
+  _gaq.push(['_trackPageview']);
+
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+
   $(function(){
     $("ul.app-menu li").each(function() {
       var href = $(this).find('a').attr("href");
@@ -8,6 +17,10 @@
       else
         if(window.location.pathname.indexOf(href) == 0 && href != '/')
           $(this).addClass("active"); 
+    });
+
+    $('a[href*=jhchabran-20]').click(function() {
+      _gaq.push(['_trackEvent','AmazonAffiliates','Link',$(this).attr('href')]);
     });
   });
 }(window.jQuery);
